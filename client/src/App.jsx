e2b93b28f735e31dd30998f1b7aa10d90ac5d1b6
Austin Killough <austinbkillough@gmail.com>
2021-03-30 11:00:40 -0700

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>testeroni</div>
+    )
+  }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(express.json());
+
+
+app.listen(port, ()=>{
+  console.log('Server listening at:', port)
+})
